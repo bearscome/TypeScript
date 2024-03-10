8969f10bc68c0e890bce4fadaab26e7416d265cf
hslee <dlgytjdsla@naver.com>
2024-03-10 20:15:07 +0900

    feat:TypeScript 기본 타입

---
+++ youtube/src/index.ts
@@ -0,0 +1,14 @@
+// 기본 타입
+
+let age:number = 30;
+let isAdult:boolean = true;
+let a:number[] = [30,3];
+let a2:Array<number> = [30];
+
+let week:string[] = ["1","2"]
+let week2:Array<string> = ["1","2"]
+
+// week.push(3); // string[]에 넘버형을 푸쉬하기 떄문에 오류
+
+
+
