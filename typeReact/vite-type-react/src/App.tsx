0fba0b2d50f37e8b1f4155a9124ad5f5f280a45a
hs.lee1 <hs.lee1@altimedia.com>
2024-11-17 22:14:22 +0900

    feat: initial push

--- typeReact/vite-type-react/src/App.tsx
+++ typeReact/vite-type-react/src/App.tsx
@@ -1,19 +1,44 @@
-import { FC } from "react";
-import "./App.css";
-import Profile from "./components/Profile";
-import Login from "./components/Login";
-import Snow from "./components/Snow";
+import {FC, useState} from 'react';
+import './App.css';
+import Store from "./Store";
+import {Address, Restaurants} from "./model/restaurants"
+import BestMenu from "./BestMenu";
 
-const App: FC = function () {
+let data:Restaurants = {
+    name: "식당",
+    category: "western",
+    address: {
+        city: "seoul",
+        detail: "somewhere",
+        zipCode: 2341,
+    },
+    menu: [
+        {
+            name: "",
+            price: 2000,
+            category: "pasta"
+        }
+    ]
+}
+const App:FC = function () {
+  const [restaurants, setRestaurants] = useState<Restaurants>(data);
+  const changeAddress = (address:Address) => {
+      setRestaurants({
+          ...restaurants,
+          address: address
+      })
+  };
+
+    const showBestMenuName = (name: string) => {
+        return name
+    }
+
   return (
-    <>
-      <div>
-        <Profile />
-        <Login />
-      </div>
-      <Snow width={100} height={100} count={20} />
-    </>
-  );
-};
+    <div>
+        <Store info = {restaurants} changeAddress = {changeAddress}/>
+        <BestMenu name = {"불고기피자"} category = {"피자"} showBestMenuName = {showBestMenuName}/>
+    </div>
+  )
+}
 
-export default App;
+export default App
